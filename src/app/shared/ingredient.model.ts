b447eeabc51e8941b1300cb13f33d2ff85bd3663
kyle <kyleghafari@gmail.com>
2020-07-27 18:21:57 -0400

    start adding component and data binding

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+//this syntax gives us the same functionality as in the recipe model - just syntatic sugar
+export class Ingredient {
+  constructor(public name: string, public amount: number) {
+  }
+}
